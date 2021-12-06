1e8463bec7c373195ad375b04903dcf246cd5607
WhenDoesSchoolEnd <95382758+WhenDoesSchoolEnd@users.noreply.github.com>
2021-12-06 15:05:45 +1300

    Updated Stuff
    + main.css
    + timer.js
    - countdown scripts from index.html

---
+++ js/timer.js
@@ -0,0 +1,51 @@
+function countdown(elm) {
+    var start = new Date;
+    start.setHours(elm.dataset.h || 0, elm.dataset.m || 0, elm.dataset.s || 0); // from dataset
+  
+    function pad(num) {
+      return ("0" + parseInt(num)).substr(-2);
+    }
+  
+    function tick() {
+      var now = new Date;
+      if (now > start) { // too late, go to tomorrow
+        start.setDate(start.getDate() + 1);
+      }
+      var remain = ((start - now) / 1000);
+      var hh = pad((remain / 60 / 60) % 60);
+      var mm = pad((remain / 60) % 60);
+      var ss = pad(remain % 60);
+      elm.innerHTML =
+        hh + ":" + mm + ":" + ss;
+      setTimeout(tick, 1000);
+    }
+    tick();
+  }
+  
+  document.querySelectorAll('.timer').forEach(countdown)
+
+  
+var countDownDate = new Date("Dec 14, 2021 00:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "School Ended!!!";
+  }
+}, 1000);
+
